c2356094192280204b1049ff56600cc75f962934
joey-kilgore <joey.a.kilgore@gmail.com>
2020-01-04 12:32:09 -0500

    update test.py

--- hardware-app/test.py
+++ hardware-app/test.py
@@ -4,29 +4,42 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 # Pin numbers for the Trigger and Echo on the Ultrasonic Sensor
-TRIG = 4
-ECHO = 18
+TRIG = 23
+ECHO = 24
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
-GPIO.output(TRIG,True)
-time.sleep(0.00001)
 GPIO.output(TRIG, False)
+print "Waiting for Sensor"
+time.sleep(2)
 
-while GPIO.input(ECHO) == False:
+while True:
+    GPIO.output(TRIG,True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+    
     start = time.time()
-
-while GPIO.input(ECHO) == True:
     end = time.time()
 
-sig_time = end - start
+    while GPIO.input(ECHO) == 0:
+        start = time.time()
+        if(start - end > 1):
+            break
 
-#cm:
-distance = sig_time / 0.000058
+    while GPIO.input(ECHO) == 1:
+        end = time.time()
+        if(end - start > 1):
+            break
 
-print ('Distance:{} cm'.format(distance))
+    sig_time = end - start
 
+    #cm:
+    distance = sig_time / 0.000058
+
+    print ('Distance:{} cm'.format(distance))
+    time.sleep(.1)
 GPIO.cleanup()
