f565507cf4952efe18522def54addf4a5a418230
Jacob <jacobhuntemail@gmail.com>
2020-01-04 00:55:28 -0500

    UI indicates whether or not space is occupied and auto-updates

--- driver-ui-app/static/script.js
+++ driver-ui-app/static/script.js
@@ -1,1 +1,27 @@
-// Javascript goes here
+// Number of milleseconds between updates
+var INTERVAL = 5000;
+
+// Function for updating the parking spaces
+function updateParkingSpaces()
+{
+    $.getJSON("./getParkingSpaces", function(data) {
+        for (var i = 0; i < data.parkingSpaceArray.length; i++)
+        {
+            console.log("boop!");
+            $("#parking-spot-" + i + " > td").html(
+                "<span class='" + (data.parkingSpaceArray[i].isOccupied ? "occupied" : "not-occupied") + "'>⬤</span>"
+            );
+        }
+    }); 
+
+}
+
+// When document is loaded...
+$(document).ready(function()
+{
+    // ... update the parking spaces
+    updateParkingSpaces();
+
+    // Keep updating the parking spaces every <INTERVAL> milliseconds
+    setInterval(updateParkingSpaces, INTERVAL);
+});
