735efa809f5c12a9acffcba809b78cf64b10f642
joey-kilgore <joey.a.kilgore@gmail.com>
2020-01-04 09:30:40 -0500

    Modify test.py

--- hardware-app/test.py
+++ hardware-app/test.py
@@ -6,51 +6,27 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Pin numbers for the Trigger and Echo on the Ultrasonic Sensor
-TRIG = 23
-ECHO = 24
+TRIG = 4
+ECHO = 18
 
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
-def distance():
-    # Ensure the Trigger pin is set low and let the sensor settle
-    GPIO.output(TRIG, False)
-    time.sleep(2)
+GPIO.output(TRIG,True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
 
-    # Trigger the module for 10 microseconds to create a short pulse.
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+while GPIO.input(ECHO) == False:
+    start = time.time()
 
-    pulse_start = time.time()
-    pulse_end = time.time()
+while GPIO.input(ECHO) == True:
+    end = time.time()
 
-    # Get the timestamp for the last low point of the Echo pin
-    while GPIO.input(ECHO) == 0:
-         pulse_start = time.time()
+sig_time = end - start
 
-    # Get the timestamp for the last high point of the Echo pin
-    while GPIO.input(ECHO) == 1:
-         pulse_end = time.time()
+#cm:
+distance = sig_time / 0.000058
 
-    # Get the elapsed time from the start and end times
-    pulse_duration = pulse_end - pulse_start
+print ('Distance:{} cm'.format(distance))
 
-    # The speed of sound at sea level is 343 m/s
-    SPEED_OF_SOUND = 34300
-
-    # Get the distance between the sensor and its object
-    distance = (pulse_duration * SPEED_OF_SOUND) / 2
-
-    return distance
-
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+GPIO.cleanup()
