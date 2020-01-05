09eab144ac6f276064ff67e9f1f20e41e0d68578
joey-kilgore <joey.a.kilgore@gmail.com>
2020-01-05 01:39:50 -0500

    Add LED functionality

--- hardware-app/button.py
+++ hardware-app/button.py
@@ -8,14 +8,16 @@ count = 0
 BUTTON_PIN = 18
 TRIG = 23
 ECHO = 24
+LED_PIN = 21
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
-
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 def button_callback():
+    GPIO.output(LED_PIN, False)
     print("Button Pressed")
     global count
     if count == 0:
@@ -30,8 +32,11 @@ def button_callback():
 try:
     global count
     while count < 11:
+        GPIO.output(LED_PIN, True)
         if GPIO.input(BUTTON_PIN) == 1:
            button_callback()
+except KeyboardInterrupt:
+   GPIO.cleanup() 
 
 except Exception:
     print("Oof")
